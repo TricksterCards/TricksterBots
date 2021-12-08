d2f680f1f0f226f3e967a803987e23462fbd6185
Ted Johnson <johnson520@gmail.com>
2021-12-08 15:31:59 -0800

    Remove reference to trumpSuit in SuggestBidState

--- WebAPI/Controllers/OhHellController.cs
+++ WebAPI/Controllers/OhHellController.cs
@@ -9,13 +9,7 @@ namespace Trickster.Bots.Controllers
         [Route("suggest/ohhell/bid")]
         public string SuggestOhHellBid([FromBody] string postData)
         {
-            return Suggester.SuggestBid<OhHellOptions>(postData, state =>
-            {
-                System.Diagnostics.Debug.WriteLineIf(state.trumpSuit != state.upCardSuit,
-                    $"state.trumpSuit is {state.trumpSuit} while state.upCardSuit is {state.upCardSuit}");
-
-                return new OhHellBot(state.options, state.upCardSuit);
-            });
+            return Suggester.SuggestBid<OhHellOptions>(postData, state => new OhHellBot(state.options, state.upCardSuit));
         }
 
         [HttpPost]
