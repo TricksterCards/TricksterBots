82dc66a3d6d16c29c7b1703566c710f26af031e8
Ted Johnson <johnson520@gmail.com>
2021-12-10 10:55:40 -0800

    Bring over WhistBot

---
+++ TricksterBots/Bots/Whist/WhistBid.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using Trickster.cloud;
+
+namespace Trickster.Bots
+{
+    public class WhistBid : IComparable<WhistBid>, IEquatable<WhistBid>
+    {
+        public const int LevelZeroTricks = 6;
+        public const int MaxLevel = 13 - LevelZeroTricks;
+        public const int MinLevel = 7 - LevelZeroTricks;
+        public static WhistBid DeclarerPartnerBid = new WhistBid(Special.DeclarerPartner);
+        public static WhistBid NotDeclareBid = new WhistBid(Special.NotDeclarer);
+
+        private static readonly Dictionary<Suit, int> BidSuitOrder = new Dictionary<Suit, int>
+        {
+            { Suit.Unknown, (int)Suit.Joker },
+            { Suit.Joker, 0 },
+            { Suit.Clubs, (int)Suit.Clubs },
+            { Suit.Spades, (int)Suit.Spades },
+            { Suit.Hearts, (int)Suit.Hearts },
+            { Suit.Diamonds, (int)Suit.Diamonds }
+        };
+
+        private readonly int theBid;
+
+        public WhistBid(int bid)
+        {
+            theBid = bid;
+        }
+
+        public WhistBid(BidBase bid)
+        {
+            theBid = bid.value;
+        }
+
+        public WhistBid(int level, bool highWins)
+            : this(Suit.Joker, level, true, highWins)
+        {
+        }
+
+        public WhistBid(Suit s, int level, bool roundOne = false, bool highWins = true)
+        {
+            if (level < MinLevel)
+                theBid = NotDeclareBid.theBid;
+            else
+            {
+                if (level > MaxLevel)
+                    level = MaxLevel;
+
+                theBid = (int)BidSpace.Whist + (level * 10 + (int)s) * 4 + MakeTwoBits(highWins, roundOne);
+            }
+        }
+
+        private WhistBid(Special special)
+        {
+            switch (special)
+            {
+                case Special.NotDeclarer:
+                    theBid = (int)BidSpace.Whist + (int)Special.NotDeclarer;
+                    break;
+                case Special.DeclarerPartner:
+                    theBid = (int)BidSpace.Whist + (int)Special.DeclarerPartner;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(special), special, null);
+            }
+        }
+
+        public bool HighWins => IsDeclareBid && (LowTwoBits & 1) == 0;
+
+        public bool IsDeclareBid => IsWhistBid && theBid != NotDeclareBid.theBid && theBid != DeclarerPartnerBid.theBid;
+
+        public bool IsDeclarePartnerBid => IsWhistBid && theBid == DeclarerPartnerBid.theBid;
+
+        public bool IsFirstRoundBid => IsDeclareBid && (LowTwoBits & 2) == 2;
+
+        public bool IsNotDeclareBid => IsWhistBid && theBid == NotDeclareBid.theBid;
+
+        public int Level => IsDeclareBid ? (theBid - (int)BidSpace.Whist) / 4 / 10 : 0;
+
+        public bool LowWins => IsDeclareBid && (LowTwoBits & 1) == 1;
+
+        public Suit Suit => IsDeclareBid ? (Suit)((theBid - (int)BidSpace.Whist) / 4 % 10) : Suit.Unknown;
+
+        public int Tricks => LevelZeroTricks + Level;
+
+        private bool IsWhistBid => theBid != BidBase.Pass && theBid != BidBase.NoBid && theBid != BidBase.NotPlaying;
+
+        private int LowTwoBits => (theBid - (int)BidSpace.Whist) % 4;
+
+        public int CompareTo(WhistBid other)
+        {
+            if (IsWhistBid && other.IsWhistBid)
+            {
+                var dif = Level.CompareTo(other.Level);
+
+                if (dif == 0)
+                    dif = IsFirstRoundBid.CompareTo(other.IsFirstRoundBid);
+
+                if (dif == 0)
+                    dif = BidSuitOrder[Suit].CompareTo(BidSuitOrder[other.Suit]);
+
+                if (dif == 0)
+                    dif = LowWins.CompareTo(other.LowWins);
+
+                return dif;
+            }
+
+            return theBid.CompareTo(other.theBid);
+        }
+
+        public bool Equals(WhistBid other)
+        {
+            return theBid.Equals(other?.theBid);
+        }
+
+        public static string GetBidText(int bid, out bool canTakePoints, out int? expectedPoints, out int? level)
+        {
+            var wb = new WhistBid(bid);
+            canTakePoints = !wb.IsDeclarePartnerBid;
+            expectedPoints = wb.IsDeclareBid ? (int?)wb.Tricks : null;
+            level = wb.IsDeclareBid && wb.IsFirstRoundBid ? (int?)wb.Level : null;
+            return wb.ToString();
+        }
+
+        public static implicit operator BidBase(WhistBid wb)
+        {
+            return new BidBase(wb.theBid);
+        }
+
+        public static implicit operator int(WhistBid wb)
+        {
+            return wb.theBid;
+        }
+
+        public string LevelSuitString()
+        {
+            var suit = Suit;
+            var suitSymbol = suit == Suit.Unknown ? "NT" : Card.SuitSymbol(suit);
+            return $"{Level}{suitSymbol}";
+        }
+
+        public override string ToString()
+        {
+            if (!IsDeclareBid)
+                return string.Empty;
+
+            if (IsFirstRoundBid)
+                return $"{Level}{(Suit == Suit.Unknown ? "NT" : HighWins ? "↑" : "↓")}";
+
+            return $"{LevelSuitString()}{(HighWins ? "↑" : "↓")}";
+        }
+
+        private static int MakeTwoBits(bool highWins, bool roundOne)
+        {
+            return (highWins ? 0 : 1) | (roundOne ? 2 : 0);
+        }
+
+        private enum Special
+        {
+            NotDeclarer,
+            DeclarerPartner
+        }
+    }
+}
---
+++ TricksterBots/Bots/Whist/WhistBot.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Trickster.cloud;
+
+namespace Trickster.Bots
+{
+    public class WhistBot : BaseBot<WhistOptions>
+    {
+        public WhistBot(WhistOptions options, Suit trumpSuit) : base(options, trumpSuit)
+        {
+        }
+
+        public override DeckType DeckType => options.DeckType;
+
+        public override BidBase SuggestBid(SuggestBidState<WhistOptions> state)
+        {
+            var hand = state.hand;
+
+            var suits = new List<Suit> { Suit.Unknown }.Concat(SuitRank.stdSuits).ToList();
+            var lowIsHigh = options._lowIsHigh; // save
+
+            options._lowIsHigh = false; // RankSort looks at this
+            var highTricksBySuit = suits.ToDictionary(s => s, s => CountTricks(hand, s));
+            var maxTrumpHighTricks = highTricksBySuit.Max(kvp => kvp.Key != Suit.Unknown ? kvp.Value : 0);
+
+            options._lowIsHigh = true; // RankSort looks at this
+            var lowTricksBySuit = suits.ToDictionary(s => s, s => CountTricks(hand, s));
+            var maxTrumpLowTricks = lowTricksBySuit.Max(kvp => kvp.Key != Suit.Unknown ? kvp.Value : 0);
+
+            var maxNotrumpTricks = Math.Max(highTricksBySuit[Suit.Unknown], lowTricksBySuit[Suit.Unknown]);
+
+            options._lowIsHigh = lowIsHigh; // restore
+
+            return state.legalBids.Where(b => b.value != BidBase.NoBid).OrderBy(b =>
+            {
+                var wb = new WhistBid(b);
+
+                if (!wb.IsDeclareBid)
+                    return -1;
+
+                //  start with 1 extra trick for the widow and 3 for partner
+                var tricks = 4;
+
+                if (options.bidderGetsKitty)
+                    tricks += 1; //  estimate one more trick if we get the kitty
+
+                if (options._highBidderSeat != -1)
+                {
+                    //  second round of bidding
+                    //  get the correct estimated tricks based on suit and whether high or low wins
+                    tricks += (wb.HighWins ? highTricksBySuit : lowTricksBySuit)[wb.Suit];
+                }
+                else
+                {
+                    //  first round of bidding
+                    if (wb.Suit == Suit.Unknown)
+                    {
+                        //  no-trump, take the best of high/low
+                        tricks += maxNotrumpTricks;
+                    }
+                    else
+                    {
+                        //  trump, take the best suit's tricks depending on whether high or low wins
+                        tricks += wb.HighWins ? maxTrumpHighTricks : maxTrumpLowTricks;
+                    }
+                }
+
+                return tricks - wb.Tricks;
+            }).Last();
+        }
+
+        public override List<Card> SuggestDiscard(SuggestDiscardState<WhistOptions> state)
+        {
+            var (player, hand) = (state.player, state.hand);
+
+            List<Card> cards;
+
+            var count = options.KittySize;
+            var theBid = new WhistBid(player.Bid);
+
+            if (theBid.Suit == Suit.Unknown)
+            {
+                //  in no-trump, throw the lowest cards we have, but make sure to get rid of Jokers first as they're useless here
+                //  TODO: try to balance this by keeping cards we need to stop a running suit
+                cards = hand.OrderBy(c => c.suit != Suit.Joker).ThenBy(RankSort).Take(count).ToList();
+            }
+            else
+            {
+                //  in trump, first group by suits to focus on creating void off-suits
+                cards = hand.GroupBy(EffectiveSuit).ToDictionary(g => g.Key, g => g.OrderBy(RankSort).ToList())
+
+                    //  save trump to discard last
+                    .OrderBy(kvp => kvp.Key == trump)
+
+                    //  try to get rid of off-suits with no cards we can make boss,
+                    .ThenBy(kvp => 0 >= HighRankInSuit(kvp.Key) - RankSort(kvp.Value.Last()) - (kvp.Value.Count - 1))
+
+                    //  followed by those that will take the longest to make a card boss
+                    .ThenByDescending(kvp => HighRankInSuit(kvp.Key) - RankSort(kvp.Value.Last()))
+
+                    //  then just get rid of the lowest cards in the shortest suits
+                    .ThenBy(kvp => kvp.Value.Count)
+
+                    //  now merge the suits into one flat list and take however many cards we need to discard
+                    .SelectMany(kvp => kvp.Value).Take(count).ToList();
+            }
+
+            return cards;
+        }
+
+        public override Card SuggestNextCard(SuggestCardState<WhistOptions> state)
+        {
+            var bid = new WhistBid(state.player.Bid);
+            return TryTakeEm(state.player,
+                state.trick,
+                state.legalCards,
+                state.cardsPlayed,
+                new PlayersCollectionBase(this, state.players),
+                state.isPartnerTakingTrick,
+                state.cardTakingTrick,
+                !bid.IsDeclareBid && !bid.IsDeclarePartnerBid);
+        }
+
+        public override List<Card> SuggestPass(SuggestPassState<WhistOptions> state)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override int RankSort(Card c, Suit trumpSuit)
+        {
+            //  high/uptown ranking is fairly straightforward
+            if (!options.IsLowHigh)
+                return (int)c.rank;
+
+            //  otherwise we have low/downtown ranking
+
+            //  here Jokers are still the most powerful cards followed by the Ace
+            if (c.rank >= Rank.Ace)
+                return (int)c.rank;
+
+            //  but then the 2 is the next most powerful and King the weakest
+            return (int)Rank.Ace - (int)c.rank + 1;
+        }
+
+        private int CountTricks(IEnumerable<Card> hand, Suit trumpSuit)
+        {
+            var deckBySuit = DeckBuilder.BuildDeck(DeckType).GroupBy(c => EffectiveSuit(c, trumpSuit)).ToDictionary(g => g.Key, g => g.OrderBy(c => RankSort(c, trumpSuit)).ToList());
+            var handBySuit = SuitRank.stdSuits.ToDictionary(s => s, s => hand.Where(c => EffectiveSuit(c, trumpSuit) == s).OrderBy(c => RankSort(c, trumpSuit)).ToList());
+
+            var tricks = 0;
+
+            if (trumpSuit == Suit.Unknown)
+            {
+                //  in no-trump, count 1 trick for each joker
+                tricks += hand.Count(c => c.suit == Suit.Joker);
+            }
+            else
+            {
+                //  trump suits are only "good" if we have at least 4 trump
+                var trumpCards = handBySuit[trumpSuit];
+                if (trumpCards.Count < 4)
+                    return 0;
+
+                //  with trump, count trump we can use on suits with singletons or voids
+                foreach (var suit in SuitRank.stdSuits.Where(s => s != trumpSuit).ToList())
+                {
+                    var countInSuit = handBySuit[suit].Count;
+                    if (countInSuit < 2)
+                    {
+                        var trumpIn = Math.Min(2 - countInSuit, trumpCards.Count);
+                        trumpCards.RemoveRange(0, trumpIn);
+                        tricks += trumpIn;
+                    }
+                }
+            }
+
+            //  then calculate the winners for each suit, accounting for gaps
+            foreach (var suit in SuitRank.stdSuits)
+            {
+                var deck = deckBySuit[suit];
+                var cards = handBySuit[suit];
+
+                var highRank = RankSort(deck.Last(), trumpSuit);
+                var nextHighestRank = highRank;
+                var hasStopper = false;
+
+                while (cards.Any())
+                {
+                    //  don't give credit for off-suit cards more than two steps below the highest rank in a trump contract
+                    //  reasoning: too easy for other players to be void and trump in by that point
+                    if (trumpSuit != Suit.Unknown && suit != trumpSuit && highRank - nextHighestRank > 2)
+                        break;
+
+                    var targetCard = cards.Last(); //  start with our next highest card
+                    var targetRank = RankSort(targetCard, trumpSuit);
+                    var gaps = deck.Count(c => targetRank < RankSort(c, trumpSuit) && RankSort(c, trumpSuit) <= nextHighestRank && !cards.Contains(c));
+                    var below = cards.Count(c => RankSort(c, trumpSuit) < targetRank);
+
+                    if (gaps > below)
+                        break;
+
+                    tricks++;
+                    hasStopper = true;
+                    nextHighestRank = targetRank;
+                    cards.Remove(targetCard);
+                    cards.RemoveRange(0, gaps);
+                }
+
+                //  if we're looking at no-trump and we don't have a stopper in all suits, bail
+                if (trumpSuit == Suit.Unknown && !hasStopper)
+                    return 0;
+            }
+
+            return tricks;
+        }
+    }
+}
