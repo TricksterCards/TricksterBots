cf1b4337edd718c2c6b62d32f76a6cf382bb50c5
Ted Johnson <johnson520@gmail.com>
2022-02-23 12:14:12 -0800

    Flesh out simple test for spades passing

--- WebAPI/Tests/SpadesPass/default.aspx.cs
+++ WebAPI/Tests/SpadesPass/default.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Text.Json;
 using System.Web.UI;
@@ -13,19 +15,34 @@ namespace Trickster.Bots.Tests.SpadesPass
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            var passState = new SuggestPassState<SpadesOptions>
-            {
-                options = new SpadesOptions { nilPass = 4 },
-                player = new PlayerBase { Hand = "ASKSQSJSTD9D8D7D6H5H4C3C2C", Bid = 0 },
-                trumpSuit = Suit.Spades,
-                passCount = 4
-            };
+            const string hand = "ASKSQSJSAD9D8D3DAH2HAC3C2C";
 
-            using (var wc = new WebClient())
+            for (var bid = 0; bid <= 1; ++bid)
             {
-                wc.Headers[HttpRequestHeader.ContentType] = "application/json";
-                var resultJson = wc.UploadString("suggest/spades/pass", JsonSerializer.Serialize(passState, _jsonSerializerOptions));
-                insertHere.Controls.Add(new HtmlGenericControl("p") { InnerText = resultJson });
+                var passState = new SuggestPassState<SpadesOptions>
+                {
+                    options = new SpadesOptions { nilPass = 4 },
+                    player = new PlayerBase { Hand = hand, Bid = bid },
+                    trumpSuit = Suit.Spades,
+                    hand = new Hand(hand),
+                    passCount = 4
+                };
+
+                var stateJson = JsonSerializer.Serialize(passState, _jsonSerializerOptions);
+                var prefix = Request.Url.GetLeftPart(UriPartial.Authority);
+
+                using (var wc = new WebClient())
+                {
+                    wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                    var resultJson = wc.UploadString($"{prefix}/suggest/spades/pass", JsonSerializer.Serialize(stateJson));
+
+                    var thePass = JsonSerializer.Deserialize<List<Card>>(JsonSerializer.Deserialize<string>(resultJson) ?? "[]");
+
+                    insertHere.Controls.Add(new HtmlGenericControl("p")
+                    {
+                        InnerText = $"Cards to be passed with bid {bid}: {string.Join(", ", thePass?.Select(c => c.ToString()) ?? new[] { "none" })}"
+                    });
+                }
             }
         }
     }
