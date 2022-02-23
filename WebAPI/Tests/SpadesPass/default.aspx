400ec122b89da5a45df7c61fe7b9d99a2d473241
Ted Johnson <johnson520@gmail.com>
2022-02-23 11:26:54 -0800

    Add test file

---
+++ WebAPI/Tests/SpadesPass/default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Trickster.Bots.Tests.SpadesPass._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Spades Passing Test</title>
+</head>
+<body>
+    <div id="insertHere" runat="server"></div>
+</body>
+</html>
---
+++ WebAPI/Tests/SpadesPass/default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using Trickster.cloud;
+
+namespace Trickster.Bots.Tests.SpadesPass
+{
+    public partial class _default : Page
+    {
+        private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions { IncludeFields = true };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var passState = new SuggestPassState<SpadesOptions>
+            {
+                options = new SpadesOptions { nilPass = 4 },
+                player = new PlayerBase { Hand = "ASKSQSJSTD9D8D7D6H5H4C3C2C", Bid = 0 },
+                trumpSuit = Suit.Spades,
+                passCount = 4
+            };
+
+            using (var wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var resultJson = wc.UploadString("suggest/spades/pass", JsonSerializer.Serialize(passState, _jsonSerializerOptions));
+                insertHere.Controls.Add(new HtmlGenericControl("p") { InnerText = resultJson });
+            }
+        }
+    }
+}
