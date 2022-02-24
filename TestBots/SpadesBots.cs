aec8331e8cf8ab7a78608a976c096637999a44c2
Ted Johnson <johnson520@gmail.com>
2022-02-23 16:58:51 -0800

    Create unit test project for bots
    Move the Spades pass tests to the unit test project

---
+++ TestBots/SpadesBots.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trickster.Bots;
+using Trickster.cloud;
+
+namespace TestBots
+{
+    [TestClass]
+    public class SpadesBots
+    {
+        private const string passingPlayerHand = "ASKSQSJSAD9D8D3DAH2HAC3C2C";
+
+        [TestMethod]
+        public void TestPassNilBid()
+        {
+            //  assert we are passing high cards for nil bid
+            Assert.AreEqual("ASKSQSAH", GetSuggestedPass(0));
+        }
+
+        [TestMethod]
+        public void TestPassNonNilBid()
+        {
+            //  assert we are passing low card for non-nil bid
+            Assert.AreEqual("2C2H3D3C", GetSuggestedPass(3));
+        }
+
+        private static string GetSuggestedPass(int bid)
+        {
+            var passState = new SuggestPassState<SpadesOptions>
+            {
+                options = new SpadesOptions { nilPass = 4 },
+                player = new PlayerBase { Hand = passingPlayerHand, Bid = bid },
+                trumpSuit = Suit.Spades,
+                hand = new Hand(passingPlayerHand),
+                passCount = 4
+            };
+
+            var bot = new SpadesBot(passState.options, passState.trumpSuit);
+            var cards = bot.SuggestPass(passState);
+            return new Hand(cards).ToString();
+        }
+    }
+}
