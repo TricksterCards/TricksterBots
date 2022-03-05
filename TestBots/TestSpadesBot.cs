da257b4dce4d15631c2804b6b5d0551783d60c7f
Ted Johnson <johnson520@gmail.com>
2022-03-05 08:45:55 -0800

    Port Spades tests from Trickster (#25)
    * Add some bid tests and one card test
    
    * Add more card tests and utility classes to simplify tests
    
    * Tweaks to spades bot tests; remove other players' hands
    
    * Add more tests; reformat code
    
    * Remove unused interface member
    
    * Refactor TestPlayer and TestCardState constructors
    
    * Correct compute seat taking trick
    
    * Port remaining card suggestion tests

--- TestBots/TestSpadesBot.cs
+++ TestBots/TestSpadesBot.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Trickster.Bots;
 using Trickster.cloud;
 
@@ -7,34 +8,335 @@ namespace TestBots
     [TestClass]
     public class TestSpadesBot
     {
-        private const string passingPlayerHand = "ASKSQSJSAD9D8D3DAH2HAC3C2C";
+        [TestMethod]
+        public void CountSureTricks()
+        {
+            var players = new[]
+            {
+                new TestPlayer(4, "QSTCTSJS", 3, cardsTaken: "7DQD5SJDAS2S6S3S3H8H7H7C"),
+                new TestPlayer(2, handScore: 2, cardsTaken: "4DAD3D2DAC2C5C3C"),
+                new TestPlayer(4, handScore: 3, cardsTaken: "8DKD5D2HKC9C4C6C8CJC7SQC"),
+                new TestPlayer(1, handScore: 1, cardsTaken: "6H4HTHAH")
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, "KS4S");
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("QS", suggestion.ToString(), $"Suggested {suggestion.StdNotation}; expected QS");
+        }
+
+        [TestMethod]
+        public void DontTakeBagsIfWeCantSetOpponents()
+        {
+            var players = new[]
+            {
+                new TestPlayer(3, "3H6H", 3),
+                new TestPlayer(2, handScore: 2),
+                new TestPlayer(3, handScore: 3),
+                new TestPlayer(2, handScore: 2)
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, "2H4H5H");
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("3H", suggestion.ToString(), "Don't take a bag when we can't set opponents");
+        }
+
+        [TestMethod]
+        public void DontTakeBagsIfWellWinAnyway()
+        {
+            var players = new[]
+            {
+                new TestPlayer(3, "3H6H", 3, 490),
+                new TestPlayer(3, handScore: 2),
+                new TestPlayer(3, handScore: 3, gameScore: 490),
+                new TestPlayer(3, handScore: 2)
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, "2H4H5H");
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("3H", suggestion.ToString(), "Don't take a bag if we'll win anyway");
+        }
+
+        [TestMethod]
+        public void DontTakeBagsIfWereTooCloseToPenalty()
+        {
+            var players = new[]
+            {
+                new TestPlayer(3, "3H6H", 3, 9),
+                new TestPlayer(3, handScore: 2),
+                new TestPlayer(3, handScore: 3, gameScore: 9),
+                new TestPlayer(3, handScore: 2)
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, "2H4H5H");
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("3H", suggestion.ToString(), "Don't take a bag if we're too close to penalty");
+        }
+
+        [TestMethod]
+        public void IgnoreNilWithHighBid()
+        {
+            var players = new[]
+            {
+                new TestPlayer(5, "AH5H4H3H2H9S8S7S6S5S4S3S2S"),
+                new TestPlayer(3),
+                new TestPlayer(5),
+                new TestPlayer(0)
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, notLegalSuit: Suit.Spades);
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("AH", suggestion.ToString(), $"Led {suggestion.StdNotation} when ignoring nil");
+        }
+
+        [TestMethod]
+        public void LeadBossToProtectNil()
+        {
+            var players = new[]
+            {
+                new TestPlayer(4, "TH9HKC8C7C6C5C4C3C2CKDKS", 1, cardsTaken: "AHKHQHJH"),
+                new TestPlayer(4),
+                new TestPlayer(0),
+                new TestPlayer(4)
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, notLegalSuit: Suit.Spades);
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("TH", suggestion.ToString(), $"Led {suggestion.StdNotation} to protect Nil");
+        }
+
+        [TestMethod]
+        public void LeadHighestLegalCardToProtectNil()
+        {
+            var players = new[]
+            {
+                new TestPlayer(4, "9H4H2HKC7C6C5C4C3C2CQDQS", 1, cardsTaken: "AHKHQHJH"),
+                new TestPlayer(4),
+                new TestPlayer(0),
+                new TestPlayer(4)
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, notLegalSuit: Suit.Spades);
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("KC", suggestion.ToString(), $"Led {suggestion.StdNotation} to protect Nil");
+        }
+
+        [TestMethod]
+        public void LeadHighestSpadeToProtectNil()
+        {
+            var players = new[]
+            {
+                new TestPlayer(4, "TH9HKC8C7C6C5C4C3CKDKS2S", 1, cardsTaken: "AHKHQHJH"),
+                new TestPlayer(4),
+                new TestPlayer(0),
+                new TestPlayer(4)
+            };
+
+            var bot = GetBot(new SpadesOptions { leadSpades = LeadSpadesWhen.Anytime });
+            var cardState = new TestCardState<SpadesOptions>(bot, players);
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("KS", suggestion.ToString(), $"Led {suggestion.StdNotation} to protect Nil");
+        }
+
+        [TestMethod]
+        public void LeadJustAbovePartnersHighestCardInSuit()
+        {
+            var players = new[]
+            {
+                new TestPlayer(4, "6H4HKC7C6C5C4C3C2CKDKS", 2, cardsTaken: "AHKHQHJHTH8H3H7H"),
+                new TestPlayer(4),
+                new TestPlayer(0) { PlayedCards = new List<PlayedCard> { new PlayedCard(new Card("TH"), new Card("3H")) } },
+                new TestPlayer(4)
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, notLegalSuit: Suit.Spades);
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("4H", suggestion.ToString(), $"Led {suggestion.StdNotation} only as high as needed");
+        }
+
+        [TestMethod]
+        public void LeadLowInPartnersVoidSuitToProtectNil()
+        {
+            var players = new[]
+            {
+                new TestPlayer(4, "4H2HKC7C6C5C4C3C2CKDKS", 2, cardsTaken: "AHKHQHJHTH5HQC3H"),
+                new TestPlayer(4),
+                new TestPlayer(0) { VoidSuits = new List<Suit> { Suit.Hearts } },
+                new TestPlayer(4)
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, notLegalSuit: Suit.Spades);
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("2H", suggestion.ToString(), $"Led {suggestion.StdNotation} to protect Nil");
+        }
+
+        [TestMethod]
+        public void OpportunisticallySetNilWithHighBid()
+        {
+            var players = new[]
+            {
+                new TestPlayer(5, "AH3HTS9S8S7S6S5S4S3S2S2C2D"),
+                new TestPlayer(3),
+                new TestPlayer(5),
+                new TestPlayer(0)
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, "2H4H5H");
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("3H", suggestion.ToString(), $"Played {suggestion.StdNotation} to get under nil bidder in 4th seat");
+        }
+
+        [TestMethod]
+        public void PlayOverHighCardToProtectNilIfNeeded()
+        {
+            var players = new[]
+            {
+                new TestPlayer(5, "2D3D4D3C4CKCQH5S7S8SJSKS"),
+                new TestPlayer(0),
+                new TestPlayer(0),
+                new TestPlayer(4)
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, "JC");
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("KC", suggestion.ToString(), $"Played {suggestion.StdNotation} to follow high to cover gaps");
+        }
+
+        [TestMethod]
+        public void SaveHighCardToProtectNilIfNoGaps()
+        {
+            var players = new[]
+            {
+                new TestPlayer(5, "2D3D4D3C4CACQH5S7S8SJSKS"),
+                new TestPlayer(2),
+                new TestPlayer(0),
+                new TestPlayer(4, handScore: 1, cardsTaken: "AH9HKH4H")
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, "KC");
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("3C", suggestion.ToString(), $"Led {suggestion.StdNotation} to follow low if no gaps");
+        }
+
+        [TestMethod]
+        public void TakeBagsIfWellEndUpShort_BustedNilCase()
+        {
+            var players = new[]
+            {
+                new TestPlayer(4, "3H6H", 4, 490),
+                new TestPlayer(4, handScore: 3),
+                new TestPlayer(0, handScore: 1, gameScore: 490),
+                new TestPlayer(4, handScore: 3)
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, "2H4H5H");
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("6H", suggestion.ToString(), "Take a bag to set opponents when we can't win this hand");
+        }
+
+        [TestMethod]
+        public void TakeBagsToSetOpponents()
+        {
+            var players = new[]
+            {
+                new TestPlayer(3, "3H6H", 3),
+                new TestPlayer(3, handScore: 2),
+                new TestPlayer(3, handScore: 3),
+                new TestPlayer(3, handScore: 2)
+            };
+
+            var bot = GetBot();
+            var cardState = new TestCardState<SpadesOptions>(bot, players, "2H4H5H");
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("6H", suggestion.ToString(), "Take a bag to set opponents");
+        }
+
+        [TestMethod]
+        public void TestBids()
+        {
+            Assert.AreEqual(4, GetSuggestedBid("2D3D5D6D8DTD  QH 2S3S5S7S9SAS", out var hand), $"Expect bid of 4 for hand {Util.PrettyHand(hand)}");
+            Assert.AreEqual(13, GetSuggestedBid("   2S3S4S5S6S7S8S9STSJSQSKSAS", out hand), $"Expect bid of 13 for hand {Util.PrettyHand(hand)}");
+            Assert.AreEqual(13, GetSuggestedBid("  AH 3S4S5S6S7S8S9STSJSQSKSAS", out hand), $"Expect bid of 13 for hand {Util.PrettyHand(hand)}");
+            Assert.AreEqual(13, GetSuggestedBid("AD AC AH 5S6S7S8S9STSJSQSKSAS", out hand), $"Expect bid of 13 for hand {Util.PrettyHand(hand)}");
+            Assert.AreEqual(0, GetSuggestedBid("2C3C4C5C6C7C8C 2D3D4D5D6D7D  ", out hand), $"Expect bid of Nil for hand {Util.PrettyHand(hand)}");
+            Assert.AreEqual(1, GetSuggestedBid("2C3C4C5C6C7C8C 2D3D4D5D6D7D  ", out hand, 0),
+                $"Expect bid of 1 for hand {Util.PrettyHand(hand)} and partner bid Nil");
+        }
 
         [TestMethod]
         public void TestPassNilBid()
         {
             //  assert we are passing high cards for nil bid
-            Assert.AreEqual("ASKSQSAH", GetSuggestedPass(0));
+            Assert.AreEqual("ASKSQSAH", GetSuggestedPass(0), "Pass high cards for Nil bid");
         }
 
         [TestMethod]
         public void TestPassNonNilBid()
         {
-            //  assert we are passing low card for non-nil bid
-            Assert.AreEqual("2C2H3D3C", GetSuggestedPass(3));
+            //  assert we are passing low cards for non-nil bid
+            Assert.AreEqual("2C2H3D3C", GetSuggestedPass(3), "Pass low cards for non-Nil bid");
+        }
+
+        private static SpadesBot GetBot()
+        {
+            return GetBot(new SpadesOptions());
+        }
+
+        private static SpadesBot GetBot(SpadesOptions options)
+        {
+            return new SpadesBot(options, Suit.Spades);
+        }
+
+        private static int GetSuggestedBid(string handString, out Hand hand, int partnerBid = BidBase.NoBid)
+        {
+            var players = new[]
+            {
+                new TestPlayer(seat: 0, hand: handString.Replace(" ", string.Empty)),
+                new TestPlayer(seat: 1),
+                new TestPlayer(seat: 2, bid: partnerBid),
+                new TestPlayer(seat: 3)
+            };
+
+            var legalBids = new List<BidBase>();
+            for (var v = 0; v <= 13; ++v) legalBids.Add(new BidBase(v));
+            legalBids.Add(new BidBase(BidBase.Pass));
+
+            hand = new Hand(players[0].Hand);
+            var bidState = new SuggestBidState<SpadesOptions>
+            {
+                player = players[0],
+                players = players,
+                legalBids = legalBids,
+                hand = hand
+            };
+
+            return GetBot().SuggestBid(bidState).value;
         }
 
         private static string GetSuggestedPass(int bid)
         {
+            const string handString = "ASKSQSJSAD9D8D3DAH2HAC3C2C";
+
             var passState = new SuggestPassState<SpadesOptions>
             {
-                options = new SpadesOptions { nilPass = 4 },
-                player = new PlayerBase { Hand = passingPlayerHand, Bid = bid },
-                trumpSuit = Suit.Spades,
-                hand = new Hand(passingPlayerHand),
+                player = new TestPlayer(bid, handString),
+                hand = new Hand(handString),
                 passCount = 4
             };
 
-            var bot = new SpadesBot(passState.options, passState.trumpSuit);
+            var bot = GetBot(new SpadesOptions { nilPass = 4 });
             var cards = bot.SuggestPass(passState);
             return new Hand(cards).ToString();
         }
---
+++ TestBots/Util.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trickster.Bots;
+using Trickster.cloud;
+
+namespace TestBots
+{
+    internal static class Util
+    {
+        public static string PrettyHand(Hand hand)
+        {
+            return string.Join(" ", hand.Select(c => c.StdNotation));
+        }
+    }
+
+    public class TestPlayer : PlayerBase
+    {
+        public TestPlayer(
+            int bid = BidBase.NoBid,
+            string hand = "",
+            int handScore = 0,
+            int gameScore = 0,
+            string cardsTaken = "",
+            int seat = 0
+        )
+        {
+            Bid = bid;
+            BidHistory = new List<int>();
+            CardsTaken = cardsTaken;
+            GameScore = gameScore;
+            Hand = hand;
+            HandScore = handScore;
+            PlayedCards = new List<PlayedCard>();
+            Seat = seat;
+            VoidSuits = new List<Suit>();
+        }
+    }
+
+    public class TestCardState<T> : SuggestCardState<T> where T : GameOptions, new()
+    {
+        public TestCardState(
+            IBaseBot bot,
+            IEnumerable<TestPlayer> players,
+            string trick = "",
+            string notLegal = "",
+            Suit notLegalSuit = Suit.Unknown
+        )
+        {
+            //  create a players collection for the valuable helpers
+            var playersCollection = new PlayersCollectionBase(bot, players);
+
+            //  ensure we have the correct number of players for the game rules
+            if (playersCollection.Count != bot.Options.players)
+                throw new Exception("Invalid number of players for game options");
+
+            //  save the underlying list into our state
+            this.players = playersCollection;
+
+            //  set the seats of the players
+            for (var seat = 0; seat < this.players.Count; ++seat)
+                this.players[seat].Seat = seat;
+
+            //  the "playing player" is assumed to be the first
+            player = this.players[0];
+
+            //  set the legal cards to be all the cards in the player's hand (adjusted later if cards in trick)
+            legalCards = new Hand(player.Hand);
+
+            //  set the cards played to the taken cards of all the players
+            cardsPlayed = new Hand(string.Join("", this.players.Select(p => p.CardsTaken)));
+
+            //  save the trick
+            this.trick = new Hand(trick);
+
+            //  if we have cards in the trick, set stuff about the trick and adjust legal cards 
+            if (this.trick.Count > 0)
+            {
+                Assert.IsTrue(this.trick.Count < playersCollection.Count, $"{this.trick.Count} cards in trick is too many");
+
+                var highCardIndex = bot.TrickHighCardIndex(this.trick);
+                cardTakingTrick = this.trick[highCardIndex];
+                var seatTakingTrick = playersCollection.Count - this.trick.Count + highCardIndex; // we assume it's seat 0's turn to play
+                isPartnerTakingTrick = playersCollection.PartnersOf(playersCollection[0]).Any(p => p.Seat == seatTakingTrick);
+                trickTaker = playersCollection.Single(p => p.Seat == seatTakingTrick);
+
+                //  if we can follow suit, we must
+                var ledSuit = bot.EffectiveSuit(this.trick[0]);
+                if (legalCards.Any(c => bot.EffectiveSuit(c) == ledSuit))
+                    legalCards = legalCards.Where(c => bot.EffectiveSuit(c) == ledSuit).ToList();
+            }
+            else
+            {
+                cardTakingTrick = null;
+                isPartnerTakingTrick = false;
+                trickTaker = null;
+
+                if (!string.IsNullOrEmpty(notLegal))
+                    legalCards = new Hand(legalCards).RemoveCards(new Hand(notLegal));
+
+                if (notLegalSuit != Suit.Unknown)
+                    legalCards = legalCards.Where(c => bot.EffectiveSuit(c) != notLegalSuit).ToList();
+            }
+        }
+    }
+}
