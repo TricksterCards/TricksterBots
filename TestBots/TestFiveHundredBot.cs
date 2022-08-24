2cd8220e588d95425c8bebb0e4468bec2cc3442d
Tony Ross <antross@gmail.com>
2022-08-24 11:33:22 -0500

    Add play-of-the-hand tests for 3-player

---
+++ TestBots/TestFiveHundredBot.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trickster.Bots;
+using Trickster.cloud;
+
+namespace TestBots
+{
+    [TestClass]
+    public class TestFiveHundredBot
+    {
+        private static readonly Suit defaultTrump = Suit.Diamonds;
+
+        private static readonly FiveHundredOptions threePlayerOptions = new FiveHundredOptions
+        {
+            isPartnership = false,
+            players = 3
+        };
+
+        [TestMethod]
+        public void SoloDucksIfEffectivePartnerTakingTrick()
+        {
+            var players = new[]
+            {
+                new TestPlayer(FiveHundredBid.NotContractorBid, "HJKD3DTH2S"),
+                new TestPlayer(new FiveHundredBid(defaultTrump, 7), gameScore: 100),
+                new TestPlayer(FiveHundredBid.NotContractorBid)
+            };
+            var bot = GetBot(defaultTrump, threePlayerOptions);
+            var cardState = new TestCardState<FiveHundredOptions>(
+                bot,
+                players,
+                trick: "8DTD"
+            );
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("3D", suggestion.ToString(), "Avoids taking trick because leader is already losing trick");
+        }
+
+        [TestMethod]
+        public void SoloTakesIfEffectivePartnerNotTakingTrick()
+        {
+            var players = new[]
+            {
+                new TestPlayer(FiveHundredBid.NotContractorBid, "HJKD3DTH2S"),
+                new TestPlayer(new FiveHundredBid(defaultTrump, 7), gameScore: 100),
+                new TestPlayer(FiveHundredBid.NotContractorBid)
+            };
+            var bot = GetBot(defaultTrump, threePlayerOptions);
+            var cardState = new TestCardState<FiveHundredOptions>(
+                bot,
+                players,
+                trick: "TD8D"
+            );
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("KD", suggestion.ToString(), "Takes trick because leader is taking trick");
+        }
+
+        [TestMethod]
+        public void SoloTakesIfNoEffectivePartner()
+        {
+            var players = new[]
+            {
+                new TestPlayer(FiveHundredBid.NotContractorBid, "HJKD3DTH2S"),
+                new TestPlayer(new FiveHundredBid(defaultTrump, 7)),
+                new TestPlayer(FiveHundredBid.NotContractorBid, gameScore: 100)
+            };
+            var bot = GetBot(defaultTrump, threePlayerOptions);
+            var cardState = new TestCardState<FiveHundredOptions>(
+                bot,
+                players,
+                trick: "8DTD"
+            );
+            var suggestion = bot.SuggestNextCard(cardState);
+            Assert.AreEqual("KD", suggestion.ToString(), "Takes trick because declarer is losing");
+        }
+
+        private static FiveHundredBot GetBot(Suit trumpSuit, FiveHundredOptions options)
+        {
+            return new FiveHundredBot(options, trumpSuit);
+        }
+    }
+}
