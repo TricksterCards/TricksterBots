b6b051312ec5f231d22df17fc568b4e5742f00d7
Tony Ross <785009+antross@users.noreply.github.com>
2023-10-26 14:22:29 -0500

    Spades: Don't bid Nil if someone bid Blind Nil
    Fix #192

--- TricksterBots/Bots/Spades/SpadesBot.cs
+++ TricksterBots/Bots/Spades/SpadesBot.cs
@@ -210,10 +210,11 @@ namespace Trickster.Bots
 
             var est = EstimatedTricks(hand);
 
-            //  try to bid Nil if the biddable bids include Nil and partner didn't already bid nil
+            //  try to bid Nil if the biddable bids include Nil, partner didn't already bid nil, and noone has bid Blind Nil
             var partner = players.PartnerOf(player);
             var partnerBidNil = partner != null && partner.Bid != BidBase.NoBid && new SpadesBid(partner.Bid).IsNil;
-            if (biddableBids.Any(b => new SpadesBid(b).IsNil))
+            var someoneBidBlindNil = players.Any(p => p.Bid != BidBase.NoBid && new SpadesBid(p.Bid).IsBlindNil);
+            if (biddableBids.Any(b => new SpadesBid(b).IsNil) && !someoneBidBlindNil)
             {
                 if (!partnerBidNil && TryNilBid(hand, est, out var bid))
                     return bid;
