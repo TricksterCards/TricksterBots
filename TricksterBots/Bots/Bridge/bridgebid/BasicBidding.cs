11fcb4f31c0f58d59ec2f19e7aefedcd1ee764cb
Tony Ross <785009+antross@users.noreply.github.com>
2023-12-12 17:44:31 -0600

    Improve types

--- TricksterBots/Bots/Bridge/bridgebid/BasicBidding.cs
+++ TricksterBots/Bots/Bridge/bridgebid/BasicBidding.cs
@@ -38,7 +38,7 @@ namespace Trickster.Bots
             return dummyPoints;
         }
 
-        public static int ComputeHighCardPoints(IEnumerable<Card> hand)
+        public static int ComputeHighCardPoints(IReadOnlyList<Card> hand)
         {
             var highCardPoints = 0;
 
