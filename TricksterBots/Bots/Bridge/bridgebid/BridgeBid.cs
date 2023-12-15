e3026bcc83b606a6340378ac0fdcb02e8ef19418
Tony Ross <785009+antross@users.noreply.github.com>
2023-12-15 13:48:41 -0600

    Add BridgeBid.HCP

--- TricksterBots/Bots/Bridge/bridgebid/BridgeBid.cs
+++ TricksterBots/Bots/Bridge/bridgebid/BridgeBid.cs
@@ -7,6 +7,7 @@ namespace Trickster.Bots
         public const int Declare = Redouble + 1; // Declare takes 9 bits (512) to encode everything
         public const int Defend = (int)BidSpace.Bridge + 1;
         public const int Double = Defend + 1;
+        public const int HCP = Declare + 0x1FF + 1; // Reserve 40 values for declaring HCP in MiniBridge
         public const int Redouble = Double + 1;
     }
 }
