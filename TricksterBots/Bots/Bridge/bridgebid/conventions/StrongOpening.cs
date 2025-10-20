b5211765de93cf01f009579f52d9811c05102b84
Tony Ross <785009+antross@users.noreply.github.com>
2025-10-20 16:33:42 -0500

    Also handle if partner has nothing

--- TricksterBots/Bots/Bridge/bridgebid/conventions/StrongOpening.cs
+++ TricksterBots/Bots/Bridge/bridgebid/conventions/StrongOpening.cs
@@ -28,6 +28,11 @@ namespace Trickster.Bots
                 return true;
             }
 
+            if (bid.Index >= 8 && bid.History[bid.Index - 8].BidConvention == BidConvention.StrongOpening)
+            {
+                return Interpret2ndRebid(bid.History[bid.Index - 4], bid.History[bid.Index - 2], bid);
+            }
+
             return false;
         }
 
@@ -136,6 +141,26 @@ namespace Trickster.Bots
                     return counts[rebid.declareBid.suit] == counts.Max(c => c.Value);
                 };
             }
+        }
+
+        private static bool Interpret2ndRebid(InterpretedBid rebid, InterpretedBid response, InterpretedBid rebid2)
+        {
+            if (response.BidConvention != BidConvention.SecondNegative)
+                return false;
+
+            if (!rebid2.bidIsDeclare || !rebid.bidIsDeclare)
+                return false;
+
+            if (rebid2.declareBid.suit != rebid.declareBid.suit)
+                return false;
+
+            if (rebid2.declareBid.level != rebid.declareBid.level + 1)
+                return false;
+
+            //  Get back to our suit if partner has nothing
+            rebid2.HandShape[rebid2.declareBid.suit] = rebid.HandShape[rebid2.declareBid.suit];
+
+            return true;
         }
 
         private static void InterpretResponderRebid(InterpretedBid response, InterpretedBid rebid, InterpretedBid responderRebid)
